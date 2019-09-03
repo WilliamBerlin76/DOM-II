b1cafbc1185fe06fdfdd9f5c9a828396c41507fb
WilliamBerlin76 <william.berlin76@gmail.com>
2019-09-03 17:39:30 -0500

    added more events

--- js/index.js
+++ js/index.js
@@ -16,10 +16,35 @@ keyDown.addEventListener('keyup', e => {
     keyDown.style.backgroundColor = 'white'
 });
 
-const busScroll = document.querySelector('img');
-busScroll.addEventListener('wheel', e => {
-    busScroll.style.transform = `scale(1.2)`;
+const busWheel = document.querySelector('img');
+busWheel.addEventListener('wheel', e => {
+    busWheel.style.transform = `scale(1.2)`;
 })
-busScroll.addEventListener('mouseleave', e => {
-    busScroll.style.transform = 'scale(1)';
+busWheel.addEventListener('mouseleave', e => {
+    busWheel.style.transform = 'scale(1)';
 })
+
+const buttonClick = document.querySelectorAll('.btn')
+buttonClick[0].addEventListener('click', e => {
+    buttonClick[0].style.backgroundColor = "purple"
+})
+buttonClick[1].addEventListener('click', e => {
+    buttonClick[1].style.backgroundColor = "purple"
+})
+buttonClick[2].addEventListener('click', e => {
+    buttonClick[2].style.backgroundColor = "purple"
+})
+
+window.addEventListener('resize', e => {
+    const changeH1 = document.querySelector('h1');
+    changeH1.textContent = "Funner Van"
+})
+const revertH1 = document.querySelector('h1')
+revertH1.addEventListener('dblclick', e => {
+    revertH1.textContent = "Fun Bus"
+})
+
+const moveBus = document.querySelector('h1')
+moveBus.addEventListener('copy', e => {
+    window.alert("You have copied the title");
+}, false);
