dedf80bd186db1d04c78713e2153738942b19638
WilliamBerlin76 <william.berlin76@gmail.com>
2019-09-03 16:15:42 -0500

    added some events

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
 // Your code goes here
+const navHover = document.querySelector('.nav');
+navHover.addEventListener("mouseover", (e) => {
+    navHover.style.backgroundColor = "red"
+});
+navHover.addEventListener('mouseleave', e =>{
+    navHover.style.backgroundColor = 'white'
+});
+const keyDown = document.querySelector('html');
+keyDown.addEventListener('keydown', e => {
+   if (e.keyCode === 82) {
+    keyDown.style.backgroundColor = 'red';
+   }
+});
+keyDown.addEventListener('keyup', e => {
+    keyDown.style.backgroundColor = 'white'
+});
+
+const busScroll = document.querySelector('img');
+busScroll.addEventListener('wheel', e => {
+    busScroll.style.transform = `scale(1.2)`;
+})
+busScroll.addEventListener('mouseleave', e => {
+    busScroll.style.transform = 'scale(1)';
+})
